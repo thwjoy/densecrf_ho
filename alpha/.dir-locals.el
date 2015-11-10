5568af4fd18be091a40bc8515518443b3e94d9b8
Rudy Bunel <bunel.rudy@gmail.com>
2015-11-10 10:22:57 +0000

    Simplify the compilation commands

--- alpha/.dir-locals.el
+++ alpha/.dir-locals.el
@@ -1,4 +1,9 @@
-((nil .  ((projectile-project-compilation-cmd . "cd build/ && cmake -GNinja ../ && ninja")
+;; Generate the first build files using:
+;;;;    cd build/ && cmake -GNinja -D CMAKE_BUILD_TYPE=Release ../ && ninja
+;;;; or cd debug/ && cmake -GNinja -D CMAKE_BUILD_TYPE=Debug ../ && ninja
+;; and then use the much simpler commands, who don't need all these arguments
+
+((nil .  ((projectile-project-compilation-cmd . "ninja -C build")
           (projectile-project-run-cmd . "build/alpha/inference")
           )
       )
