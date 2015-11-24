f89aac0249755621dd3969a89798a6ef7c446c6f
Rudy Bunel <bunel.rudy@gmail.com>
2015-11-24 11:28:50 +0000

    Change the image for the example inference
    The previous one was from the train set.

--- alpha/example_inference.cpp
+++ alpha/example_inference.cpp
@@ -2,14 +2,16 @@
 #include "inference.hpp"
 
 int main(int argc, char* argv[]) {
-    std::string path_to_unaries = "/home/rudy/datasets/MSRC/texton_unaries/2_21_s.c_unary";
+    std::string path_to_unaries = "/home/rudy/datasets/MSRC/texton_unaries/2_27_s.c_unary";
     std::string path_to_output = "/home/rudy/workspace/densecrf/build/res.bmp";
     std::string path_to_mf_output = "/home/rudy/workspace/densecrf/build/res-mf.bmp";
-    std::string path_to_image = "/home/rudy/datasets/MSRC/MSRC_ObjCategImageDatabase_v2/Images/2_21_s.bmp";
+    std::string path_to_unoutput = "/home/rudy/workspace/densecrf/build/res-un.bmp";
+    // the image that we are using is from the validation set.
+    std::string path_to_image = "/home/rudy/datasets/MSRC/MSRC_ObjCategImageDatabase_v2/Images/2_27_s.bmp";
 
-    std::string path_to_parameters = "/home/rudy/workspace/densecrf/clang/alpha/latest_parameters.csv";
+    std::string path_to_parameters = "/home/rudy/workspace/densecrf/build/alpha/learned_parameters.csv";
 
-    minimize_dense_alpha_divergence(path_to_image, path_to_unaries, path_to_output, path_to_parameters, 20);
-
+    minimize_dense_alpha_divergence(path_to_image, path_to_unaries, path_to_output, path_to_parameters, 10);
+    unaries_baseline(path_to_unaries, path_to_unoutput);
     minimize_mean_field(path_to_image, path_to_unaries,  path_to_mf_output, path_to_parameters);
 }
