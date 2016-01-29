94174115f7bb0e0bcaa5d07e91940b8631755c7c
albanD <alban@robots.ox.ac.uk>
2016-01-29 16:01:54 +0000

    change all path to /data

--- alpha/example_inference.cpp
+++ alpha/example_inference.cpp
@@ -2,16 +2,16 @@
 #include "inference.hpp"
 
 int main(int argc, char* argv[]) {
-    std::string path_to_unaries = "/home/rudy/datasets/MSRC/texton_unaries/2_27_s.c_unary";
-    std::string path_to_alpha_output = "/home/rudy/workspace/densecrf/build/res.bmp";
-    std::string path_to_mf_output = "/home/rudy/workspace/densecrf/build/res-mf.bmp";
-    std::string path_to_cccp_output = "/home/rudy/workspace/densecrf/build/res-cccp-mf.bmp";
-    std::string path_to_unoutput = "/home/rudy/workspace/densecrf/build/res-un.bmp";
-    std::string path_to_qpoutput = "/home/rudy/workspace/densecrf/build/res-lrqp.bmp";
+    std::string path_to_unaries = "/data/MSRC/texton_unaries/2_27_s.c_unary";
+    std::string path_to_alpha_output = "/data/densecrf/res.bmp";
+    std::string path_to_mf_output = "/data/densecrf/res-mf.bmp";
+    std::string path_to_cccp_output = "/data/densecrf/res-cccp-mf.bmp";
+    std::string path_to_unoutput = "/data/densecrf/res-un.bmp";
+    std::string path_to_qpoutput = "/data/densecrf/res-lrqp.bmp";
     // the image that we are using is from the validation set.
-    std::string path_to_image = "/home/rudy/datasets/MSRC/MSRC_ObjCategImageDatabase_v2/Images/2_27_s.bmp";
+    std::string path_to_image = "/data/MSRC/MSRC_ObjCategImageDatabase_v2/Images/2_27_s.bmp";
 
-    std::string path_to_parameters = "/home/rudy/workspace/densecrf/build/alpha/learned_parameters.csv";
+    std::string path_to_parameters = "/data/densecrf/alpha/learned_parameters.csv";
 
 
 
