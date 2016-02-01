30ceda5fe49df913eacf260c0287af3cbc296483
Rudy Bunel <bunel.rudy@gmail.com>
2016-02-01 11:50:55 +0000

    QP CCCP  boilerplate

--- alpha/example_inference.cpp
+++ alpha/example_inference.cpp
@@ -7,7 +7,8 @@ int main(int argc, char* argv[]) {
     std::string path_to_mf_output = "/data/densecrf/res-mf.bmp";
     std::string path_to_cccp_output = "/data/densecrf/res-cccp-mf.bmp";
     std::string path_to_unoutput = "/data/densecrf/res-un.bmp";
-    std::string path_to_qpoutput = "/data/densecrf/res-lrqp.bmp";
+    std::string path_to_qplroutput = "/data/densecrf/res-lrqp.bmp";
+    std::string path_to_qpcccp_output = "/data/densecrf/res-cccp-qp.bmp";
     // the image that we are using is from the validation set.
     std::string path_to_image = "/data/MSRC/MSRC_ObjCategImageDatabase_v2/Images/2_27_s.bmp";
 
@@ -19,5 +20,6 @@ int main(int argc, char* argv[]) {
     // unaries_baseline(path_to_unaries, path_to_unoutput);
     // minimize_mean_field(path_to_image, path_to_unaries,  path_to_mf_output, path_to_parameters);
     // minimize_cccp_mean_field(path_to_image, path_to_unaries, path_to_cccp_output, path_to_parameters);
-    minimize_LR_QP(path_to_image, path_to_unaries, path_to_qpoutput, path_to_parameters);
+    // minimize_LR_QP(path_to_image, path_to_unaries, path_to_qplroutput, path_to_parameters);
+    minimize_QP_cccp(path_to_image, path_to_unaries, path_to_qpcccp_output, path_to_parameters);
 }
--- alpha/inference.cpp
+++ alpha/inference.cpp
@@ -90,6 +90,35 @@ void minimize_LR_QP(std::string path_to_image, std::string path_to_unaries,
     save_map(Q, size, path_to_output);
 }
 
+void minimize_QP_cccp(std::string path_to_image, std::string path_to_unaries,
+                      std::string path_to_output, std::string path_to_parameters) {
+    img_size size;
+    // Load the unaries potentials for our image.
+    MatrixXf unaries = load_unary(path_to_unaries, size);
+    unsigned char * img = load_image(path_to_image, size);
+    VectorXf pairwise_parameters = load_matrix(path_to_parameters);
+
+    // Load a crf
+    DenseCRF2D crf(size.width, size.height, unaries.rows());
+
+    int M = unaries.rows();
+    crf.setUnaryEnergy(unaries);
+    crf.addPairwiseGaussian(3,3, new PottsCompatibility(3));
+    crf.addPairwiseBilateral(40,40,2.5,2.5,2.5, img, new PottsCompatibility(3.5));
+    //crf.compute_kl_divergence();
+
+    clock_t start, end;
+    start = clock();
+    MatrixXf Q = crf.qp_cccp_inference();
+    end = clock();
+    double timing = (double(end-start)/CLOCKS_PER_SEC);
+    // std::cout << "Time taken: " << timing << '\n';
+    // std::cout << "Done with inference"<< '\n';
+    // Perform the MAP estimation on the fully factorized distribution
+    // and write the results to an image file with a dumb color code
+    save_map(Q, size, path_to_output);
+}
+
 
 
 void minimize_cccp_mean_field(std::string path_to_image, std::string path_to_unaries,
