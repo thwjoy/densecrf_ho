f7cb056b8da107428d391282fb0a921dc97f7176
Rudy Bunel <bunel.rudy@gmail.com>
2016-02-06 13:21:38 +0000

    Possible to example test on any dataset

--- alpha/example_inference.cpp
+++ alpha/example_inference.cpp
@@ -2,17 +2,25 @@
 #include "inference.hpp"
 
 int main(int argc, char* argv[]) {
-    std::string path_to_unaries = "/data/MSRC/texton_unaries/2_27_s.c_unary";
+    std::string dataset_name = "Pascal2010";
+    std::string path_to_unaries;
+    std::string path_to_image;
+    if (dataset_name=="MSRC") {
+        path_to_unaries = "/data/MSRC/texton_unaries/2_27_s.c_unary";
+        path_to_image = "/data/MSRC/MSRC_ObjCategImageDatabase_v2/Images/2_27_s.bmp";
+    } else if(dataset_name == "Pascal2010"){
+        path_to_unaries = "/data/PascalVOC2010/unaries/2010_003547.c_unary";
+        path_to_image = "/data/PascalVOC2010/JPEGImages/2010_003547.jpg";
+    }
+
+
     std::string path_to_alpha_output = "/data/densecrf/res.bmp";
     std::string path_to_mf_output = "/data/densecrf/res-mf.bmp";
     std::string path_to_cccp_output = "/data/densecrf/res-cccp-mf.bmp";
     std::string path_to_unoutput = "/data/densecrf/res-un.bmp";
     std::string path_to_qplroutput = "/data/densecrf/res-lrqp.bmp";
     std::string path_to_qpcccp_output = "/data/densecrf/res-cccp-qp.bmp";
-    std::string dataset_name = "MSRC";
     // the image that we are using is from the validation set.
-    std::string path_to_image = "/data/MSRC/MSRC_ObjCategImageDatabase_v2/Images/2_27_s.bmp";
-
     std::string path_to_parameters = "/data/densecrf/alpha/learned_parameters.csv";
 
     Potts_weight_set params(3, 3, 50, 15, 5);
