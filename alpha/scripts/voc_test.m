6058aad80a20329f7bd0920cb71f9187a11da928
Rudy Bunel <bunel.rudy@gmail.com>
2016-02-10 17:01:43 +0000

    Small changes to the Voctest.m script

--- alpha/scripts/voc_test.m
+++ alpha/scripts/voc_test.m
@@ -2,18 +2,18 @@ addpath('/data/PascalVOC2010/VOCdevkit/VOCcode');
 
 VOCinit;
 
+task = 'mf';
+results_folder = 'Test'
 
-task = 'mfiter';
 
 
 VOCopts.datadir = '/data/PascalVOC2010';
-VOCopts.resdir = ['/data/Results/Pascal2010/Test/' task '/'];
 VOCopts.testset = 'Test';
 
 
-VOCopts.seg.clsimgpath='/data/PascalVOC2010/SegmentationClass/%s.png';
-VOCopts.seg.imgsetpath='/data/PascalVOC2010/split/%s.txt';
-VOCopts.seg.clsresdir='/data/Results/Pascal2010/Test/%s';
+VOCopts.seg.clsimgpath= '/data/PascalVOC2010/SegmentationClass/%s.png';
+VOCopts.seg.imgsetpath= '/data/PascalVOC2010/split/%s.txt';
+VOCopts.seg.clsresdir= ['/data/Results/Pascal2010/' results_folder '/%s'];
 VOCopts.seg.clsrespath=[VOCopts.seg.clsresdir '/%s.png'];
 
 
