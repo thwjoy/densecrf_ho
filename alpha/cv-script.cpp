4749b9ff636a8214ae03a62ee60bdbf0bfea6358
Rudy Bunel <bunel.rudy@gmail.com>
2016-02-25 09:32:50 +0000

    Experiments

--- alpha/cv-script.cpp
+++ alpha/cv-script.cpp
@@ -5,7 +5,7 @@
 #include "densecrf.h"
 
 void image_inference(Dataset dataset, std::string path_to_results,
-                     std::string image_name, float pairweight)
+                     std::string image_name, float spc_std, float spc_potts, float bil_spcstd, float bil_colstd, float bil_potts)
 {
 
     std::string image_path = dataset.get_image_path(image_name);
@@ -19,51 +19,16 @@ void image_inference(Dataset dataset, std::string path_to_results,
 
     DenseCRF2D crf(size.width, size.height, unaries.rows());
     crf.setUnaryEnergy(unaries);
-    crf.addPairwiseBilateral(50, 50,
-                             15, 15, 15,
-                             img, new PottsCompatibility(pairweight));
+    crf.addPairwiseGaussian(spc_std, spc_std, new PottsCompatibility(spc_potts));
+    crf.addPairwiseBilateral(bil_spcstd, bil_spcstd,
+                             bil_colstd, bil_colstd, bil_colstd,
+                             img, new PottsCompatibility(bil_potts));
     MatrixXf Q;
-    // Run Meanfield for 5 iterations
     {
-        std::string path_to_subexp_results = path_to_results + "/mf5/";
+        std::string path_to_subexp_results = path_to_results + "/lrqp/";
         std::string output_path = get_output_path(path_to_subexp_results, image_name);
         if (not file_exist(output_path)) {
-            Q = crf.inference(5);
-            make_dir(path_to_subexp_results);
-            double final_energy = crf.compute_energy(Q);
-            double discretized_energy = crf.assignment_energy(crf.currentMap(Q));
-            save_map(Q, size, output_path, dataset_name);
-            std::string txt_output = output_path;
-            txt_output.replace(txt_output.end()-3, txt_output.end(),"txt");
-            std::ofstream txt_file(txt_output.c_str());
-            txt_file << 0 << '\t' << final_energy << '\t' << discretized_energy << std::endl;
-            txt_file.close();
-        }
-    }
-
-    // Run Meanfield for 15 iterations
-    {
-        std::string path_to_subexp_results = path_to_results + "/mf15/";
-        std::string output_path = get_output_path(path_to_subexp_results, image_name);
-        if (not file_exist(output_path)) {
-            Q = crf.inference(15);
-            make_dir(path_to_subexp_results);
-            double final_energy = crf.compute_energy(Q);
-            double discretized_energy = crf.assignment_energy(crf.currentMap(Q));
-            save_map(Q, size, output_path, dataset_name);
-            std::string txt_output = output_path;
-            txt_output.replace(txt_output.end()-3, txt_output.end(),"txt");
-            std::ofstream txt_file(txt_output.c_str());
-            txt_file << 0 << '\t' << final_energy << '\t' << discretized_energy << std::endl;
-            txt_file.close();
-        }
-    }
-    // Run Meanfield for 25 iterations
-    {
-        std::string path_to_subexp_results = path_to_results + "/mf25/";
-        std::string output_path = get_output_path(path_to_subexp_results, image_name);
-        if (not file_exist(output_path)) {
-            Q = crf.inference(25);
+            Q = crf.qp_inference();
             make_dir(path_to_subexp_results);
             double final_energy = crf.compute_energy(Q);
             double discretized_energy = crf.assignment_energy(crf.currentMap(Q));
@@ -90,7 +55,15 @@ int main(int argc, char *argv[])
     std::string path_to_results = argv[3];
 
     std::string param1 = argv[4];
-    float pair_weight = std::stof(param1);
+    float spc_std = std::stof(param1);
+    std::string param2 = argv[5];
+    float spc_potts = std::stof(param2);
+    std::string param3 = argv[6];
+    float bil_spcstd = std::stof(param3);
+    std::string param4 = argv[7];
+    float bil_colstd = std::stof(param4);
+    std::string param5 = argv[8];
+    float bil_potts = std::stof(param5);
 
     make_dir(path_to_results);
 
@@ -99,7 +72,8 @@ int main(int argc, char *argv[])
     omp_set_num_threads(8);
 #pragma omp parallel for
     for(int i=0; i< test_images.size(); ++i){
-        image_inference(ds, path_to_results, test_images[i], pair_weight);
+        image_inference(ds, path_to_results, test_images[i], spc_std, spc_potts,
+                        bil_spcstd, bil_colstd, bil_potts);
     }
 
 
