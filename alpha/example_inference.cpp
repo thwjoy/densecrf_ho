f90dcb9cdc6700fb6cc2995659942c73d4907112
Rudy Bunel <bunel.rudy@gmail.com>
2016-03-03 17:28:12 +0000

    Setup to debug the QP being non deterministic

--- alpha/example_inference.cpp
+++ alpha/example_inference.cpp
@@ -2,15 +2,15 @@
 #include "inference.hpp"
 
 int main(int argc, char* argv[]) {
-    std::string dataset_name = "MSRC";
+    std::string dataset_name = "Pascal2010";
     std::string path_to_unaries;
     std::string path_to_image;
     if (dataset_name=="MSRC") {
         path_to_unaries = "/data/MSRC/texton_unaries/2_14_s.c_unary";
         path_to_image = "/data/MSRC/MSRC_ObjCategImageDatabase_v2/Images/2_14_s.bmp";
     } else if(dataset_name == "Pascal2010"){
-        path_to_unaries = "/data/PascalVOC2010/unaries/2007_007470.c_unary";
-        path_to_image = "/data/PascalVOC2010/JPEGImages/2007_007470.jpg";
+        path_to_unaries = "/data/PascalVOC2010/logit_unaries/2007_006704.c_unary";
+        path_to_image = "/data/PascalVOC2010/JPEGImages/2007_006704.jpg";
     }
 
 
@@ -18,13 +18,13 @@ int main(int argc, char* argv[]) {
     // std::string path_to_mf_output = "/data/densecrf/res-mf.bmp";
     // std::string path_to_cccp_output = "/data/densecrf/res-cccp-mf.bmp";
     // std::string path_to_unoutput = "/data/densecrf/res-un.bmp";
-    // std::string path_to_qplroutput = "/data/densecrf/res-lrqp.bmp";
+    std::string path_to_qplroutput = "/data/densecrf/res-lrqp.bmp";
     // std::string path_to_qpcccp_output = "/data/densecrf/res-cccp-qp.bmp";
     // std::string path_to_fixed_iter_output = "/data/densecrf/res-fixediter-mf.bmp";
-    std::string path_to_lp_output = "./res-lp.bmp";
+    // std::string path_to_lp_output = "./res-lp.bmp";
     // the image that we are using is from the validation set.
 
-    Potts_weight_set params(1, 0.5, 1, 95.208, 100);
+    Potts_weight_set params(1, 0.5, 1, 100, 95.2085068051);
 
     // minimize_dense_alpha_divergence(path_to_image, path_to_unaries, path_to_alpha_output, path_to_parameters, 5);n
     // std::cout << "Unaries" << '\n';
@@ -35,10 +35,10 @@ int main(int argc, char* argv[]) {
     // minimize_cccp_mean_field(path_to_image, path_to_unaries, params, path_to_cccp_output, dataset_name);
     // std::cout << "Fixed Iter Meanfield"  << '\n';
     // minimize_mean_field_fixed_iter(path_to_image, path_to_unaries, params, path_to_fixed_iter_output, dataset_name, 5);
-    // std::cout << "Lafferty QP"  << '\n';
-    // minimize_LR_QP(path_to_image, path_to_unaries, params, path_to_qplroutput, dataset_name);
+    std::cout << "Lafferty QP"  << '\n';
+    minimize_LR_QP(path_to_image, path_to_unaries, params, path_to_qplroutput, dataset_name);
     // std::cout << "CCCP QP" << '\n';
     // minimize_QP_cccp(path_to_image, path_to_unaries, params, path_to_qpcccp_output, dataset_name);
-    std::cout << "LP" << '\n';
-    minimize_LP(path_to_image, path_to_unaries, params, path_to_lp_output, dataset_name);
+    // std::cout << "LP" << '\n';
+    // minimize_LP(path_to_image, path_to_unaries, params, path_to_lp_output, dataset_name);
 }
