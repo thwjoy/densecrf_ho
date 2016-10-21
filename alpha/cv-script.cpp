a81c59eb03693e4fffdf49e952f4ba2a7cf509e7
Ajanthan <ajaneng@gmail.com>
2016-10-21 19:00:30 +0100

    1. omp parallel for is slower than sequential for! --> need to fix this

--- alpha/cv-script.cpp
+++ alpha/cv-script.cpp
@@ -188,10 +188,10 @@ int main(int argc, char *argv[])
 
     Dataset ds = get_dataset_by_name(dataset_name);
     std::vector<std::string> test_images = ds.get_all_split_files(dataset_split);
-    omp_set_num_threads(1);
-#pragma omp parallel for
-    //for(int i=0; i< test_images.size(); ++i){
-    for(int i=0; i< 10; ++i){
+    //omp_set_num_threads(1);
+//#pragma omp parallel for
+    for(int i=0; i< test_images.size(); ++i){
+    //for(int i=0; i< 10; ++i){
         image_inference(ds, method, path_to_results,  test_images[i], spc_std, spc_potts,
                         bil_spcstd, bil_colstd, bil_potts, lp_params);
     }
